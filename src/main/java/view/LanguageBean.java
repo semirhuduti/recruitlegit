e5e13c39a0012897e4f897aae3fb61f04b5dcbf6
Semir <Semir@Semir-PC>
2014-02-19 14:46:20 +0100

    Merge commit 'f84850b2b79b78cc1df72a6e5c124eab83a143e3'
    Conflicts:
    	src/testgit/test.java

---
+++ src/main/java/view/LanguageBean.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
+import javax.annotation.PostConstruct;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+
+/**
+ *
+ * @author Semir
+ */
+@Named("languageManager")
+@SessionScoped
+public class LanguageBean implements Serializable {
+
+    private Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public String getLanguage() {
+        return locale.getLanguage();
+    }
+
+    public void setLanguage(String language) {
+        locale = new Locale(language);
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+    }
+
+}
